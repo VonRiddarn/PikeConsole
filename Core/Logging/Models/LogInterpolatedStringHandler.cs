f223c690c565b1fff224b1d9ee49d7470655ff12
VonRiddarn <vonriddarn@gmail.com>
2026-06-05 01:20:19 +0200

    feat: add custom string builder for interpolatied logs NOTE: This will, from what I can gather, help with memory allocation. Making the logger more or less zero alloc if the runtime killswitch is on.

---
+++ Core/Logging/Models/LogInterpolatedStringHandler.cs
@@ -0,0 +1,37 @@
+using System.Runtime.CompilerServices;
+using FractalPike.PikeConsole.Core.Logging;
+using FractalPike.PikeConsole.Core.Logging.Models;
+
+[InterpolatedStringHandler]
+public ref struct LogInterpolatedStringHandler
+{
+	DefaultInterpolatedStringHandler _handler;
+	public bool IsEnabled { get; }
+
+	public LogInterpolatedStringHandler(int literalLength, int formattedCount, LogTarget target)
+	{
+		IsEnabled = PikeLogger.IsTargetEnabled(target);
+
+		if (IsEnabled)
+		{
+			_handler = new DefaultInterpolatedStringHandler(literalLength, formattedCount);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void AppendLiteral(string value)
+	{
+		if (IsEnabled) _handler.AppendLiteral(value);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void AppendFormatted<T>(T value)
+	{
+		if (IsEnabled) _handler.AppendFormatted(value);
+	}
+
+	internal string ToStringAndClear()
+	{
+		return IsEnabled ? _handler.ToStringAndClear() : string.Empty;
+	}
+}
