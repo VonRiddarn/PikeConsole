80121c4159d020cae6fa4c4efb13c2ca185a24d8
VonRiddarn <vonriddarn@gmail.com>
2026-06-06 18:23:40 +0200

    feat: add ExecutionResponseStatus to RuntimeExecution system

---
+++ Core/RuntimeExecution/Models/ExecutionResponseStatus.cs
@@ -0,0 +1,19 @@
+namespace FractalPike.PikeConsole.Core.RuntimeExecution;
+
+public enum ExecutionResponseStatus
+{
+	/// <summary>Default fallback.</summary>
+	None = 0,
+	/// <summary>Executed expectedly.</summary>
+	Success = 1,
+	/// <summary>Denied because of cheat flag.</summary>
+	DeniedCheat = 2,
+	/// <summary>Denied because of permission system.</summary>
+	DeniedPermission = 3,
+	/// <summary>Denied because of invalid arguments.</summary>
+	InvalidArgs = 4,
+	/// <summary>Failed expectedly (IE: Through gameplay context).</summary>
+	Failed = 5,
+	/// <summary>Failed unexpectedly.</summary>
+	Error = 6
+}
