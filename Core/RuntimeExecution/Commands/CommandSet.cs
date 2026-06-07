de2aa559bafde512b4880da69efeb0bb22aa81b5
VonRiddarn <vonriddarn@gmail.com>
2026-06-07 13:53:13 +0200

    feat: port CommnadSet base from Unity based framework NOTE TO SELF: Lots of internal changes has been made to ownership and responsibility. The CommandSet is now self-diagnostic with regards to what child messed up.

--- Core/RuntimeExecution/Commands/CommandSet.cs
+++ Core/RuntimeExecution/Commands/CommandSet.cs
@@ -1,30 +1,125 @@
+using System;
+using System.Runtime.CompilerServices;
+using FractalPike.PikeConsole.Core.Logging;
 using FractalPike.PikeConsole.Core.RuntimeExecution;
 using Godot;
 
 public abstract partial class CommandSet : Node
 {
+	/// <summary>
+	/// All commands within this CommandSet.
+	/// Initialized only once at runtime.
+	/// </summary>
+	public Command[] Commands { get; private set; } = [];
 
-	// TODO: IMPORTANT NOTE - DO NOT FORGET TO SEND COMPILER INJECTION ATTRIBUTES FOR FILEPATH TO COMMAND.
-	// Since this is a helper, we must send the creator of the command to the logger for error diagnostics.
-	// Look over the Unity based framework before doing anything.
+	// ----- ----- GODOT API WRAPPER ----- -----
+	// Wrapper virtual API for Godot methods.
+	// This is so that it is exceptionally clear that the original methods are off limits.
+	protected virtual void OnEnterTree() { }
+	protected virtual void OnReady() { }
+	protected virtual void OnExitTree() { }
 
-	public IRuntimeExecutable[] Commands { get; private set; } = [];
+	/// <summary>
+	/// Obligatory registration method that must return an array of Commands.
+	/// This is used by internal systems to register commands to the registry.
+	/// Example usage:
+	/// <code>
+	/// protected override Command[] InstantiateCommands() => [
+	/// Command(...),
+	/// Command(...)
+	/// ];
+	/// </code>
+	/// </summary>
+	/// <remarks>
+	/// Use with the shorthand method <c>Command()</c> to get automatic stack trace injection.
+	/// This makes debugging easier!
+	/// </remarks>
+	protected abstract Command[] InstantiateCommands();
 
-	// _EnterTree is like Awake()?
-	public override void _EnterTree()
+
+	// ----- ----- GODOT API ----- -----
+	// Sealing important initialization methods so that nobody accidentally 
+	// overrides them without calling base..().
+	public sealed override void _EnterTree()
 	{
-		// InitializeCommandsInternalOneShot();
+		if (Commands.Length <= 0)
+			InitializeCommandsInternal();
+
+		OnEnterTree();
 	}
 
-	// _Ready is like Start()?
-	public override void _Ready()
+	public sealed override void _Ready()
 	{
-		// RegisterCommands(Commands);
+		CommandRegistry.Register(Commands);
+		OnReady();
 	}
 
-	// _ExitTree is like OnDestroy()?
-	public override void _ExitTree()
+	public sealed override void _ExitTree()
 	{
-		CommandRegistry.UnRegister(Commands);
+		CommandRegistry.Unregister(Commands);
+		OnExitTree();
+	}
+
+	// ----- ----- INHERITED API ----- -----
+	/// <summary>
+	/// Shorthand for creating commands.
+	/// This will automatically inject a custom stack trace to the caller CommandSet 
+	/// (compile time, no overhead) to the created command.
+	/// </summary>
+	/// <param name="signature">
+	/// Command signature to run this executable, EG: "r_cleardecals" or "env_gc"<br/>
+	/// A signature has no set naming rules, but it is convention to name it after scope separated using underscores.
+	/// </param>
+	/// <param name="shortDesc">The short description (1 line) for this executable. EG: "Force-activates the garbage collector"</param>
+	/// <param name="longDesc">The long description for this executable. Go wild.</param>
+	/// <param name="usage">Usage example of this executable, EG: "em_find [enemy id | enemy type]" or "env_gc [no args]"</param>
+	/// <param name="isCheat">If set to true CheatMode must be active to run this command.</param>
+	/// <param name="action">
+	/// The method to run when this command is called. Example of a simple echo command:
+	/// <code>
+	/// (args) => {
+	///     PikeLogger.Log(LogTarget.Runtime, $"{string.Join(' ', args)}");
+	///     return new Response&lt;ExecutionResponseStatus&gt;(ExecutionResponseStatus.Success);
+	/// }
+	/// </code>
+	/// </param>
+	/// <param name="filePath">COMPILER INJECTED ARGUMENT, DO NOT SET.</param>
+	/// <param name="lineNumber">COMPILER INJECTED ARGUMENT, DO NOT SET.</param>
+	/// <returns>A command with an automatic compile-time custom stacktrace.</returns>
+	protected Command Command(
+		string signature,
+		string shortDesc,
+		string longDesc,
+		string usage,
+		bool isCheat,
+		Func<string[], Response<ExecutionResponseStatus>> action,
+		[CallerFilePath] string filePath = "",
+		[CallerLineNumber] int lineNumber = 0)
+	{
+		return new Command(signature, shortDesc, longDesc, usage, isCheat, action, new CustomStackTrace(filePath, lineNumber));
+	}
+
+
+	// ----- ----- INTERNAL API ----- -----
+
+	// We inject the filepath and linenumber so that we can reference the actual child that messed up instead of this base class.
+	// This class is self-diagnostic by design. This ensures we fail fast and catch collisions early.
+	void InitializeCommandsInternal(
+		[CallerFilePath] string filePath = "",
+		[CallerLineNumber] int lineNumber = 0
+	)
+	{
+		try
+		{
+			Commands = InstantiateCommands() ?? [];
+		}
+		catch (Exception ex)
+		{
+			Commands = [];
+			PikeLogger.LogError(LogTarget.All, $"UNEXPECTED ERROR: Failed to instantiate commands in {filePath}:{lineNumber} [ Node: {this}] - {ex}");
+		}
+
+		if (Commands.Length <= 0)
+			PikeLogger.LogWarning(LogTarget.All, $"Commands failed to register from {filePath}:{lineNumber} [Node: {this}]. Make sure InstantiateCommands return a valid array!");
 	}
 }
