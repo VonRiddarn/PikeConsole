5a47431963f6b71e12af6180a639b61d95f77330
VonRiddarn <vonriddarn@gmail.com>
2026-06-08 23:19:09 +0200

    feat: make signature parser more defensive

--- Core/RuntimeExecution/Extentions/Signatures.cs
+++ Core/RuntimeExecution/Extentions/Signatures.cs
@@ -4,5 +4,5 @@ public static class StringExtensions
 {
 	// Not super strict or anything, but this just trims the signature and turns spaces into underscores.
 	// The statement parser separates commands from arguments with spaces, so it's important the signature is valid.
-	public static string ToSignature(this string str) => str.Trim().ToLower().Replace(' ', '_');
+	public static string ToSignature(this string str) => !string.IsNullOrWhiteSpace(str) ? str.Trim().ToLower().Replace(' ', '_') : "";
 }
