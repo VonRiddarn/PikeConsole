5788ca5181fc9e5c06878e2147f1289bcc459885
VonRiddarn <vonriddarn@gmail.com>
2026-06-11 01:21:34 +0200

    feat: port CVar base from the old Unity based framework IMPROVEMENTS: - Rename ValueAsString to DisplayValue for semantic clarity - Remove middle-man method GetValues() - Move logging responsibility from execution method. Execute only returns a response that can be dealt with however - Execute is now fully self-diagnostic (through said responses) - InitializeInternal is now fully self-diagnostic (through direct logging on errors) - More explicit separation of concerns between data and DisplayData - Rename to CvarBase from CVarInternal

---
+++ Core/RuntimeExecution/CVars/CVarBase.cs
@@ -0,0 +1,230 @@
+using System;
+using System.Collections.Generic;
+using FractalPike.PikeConsole.Core.Extensions;
+using FractalPike.PikeConsole.Core.Logging;
+using Godot;
+namespace FractalPike.PikeConsole.Core.RuntimeExecution;
+
+#nullable enable
+
+public abstract partial class CVarBase<T> : Resource, ICVar
+{
+	/// <summary>
+	/// Used for subscribing to the CVar value. If the value changes, this event fires and sends the new value.
+	/// </summary>
+	public event Action<T>? ValueChanged;
+	/// <summary>
+	/// Used for subscribing on the Cvar state. If the value changes, this event fires but sends nothing.
+	/// </summary>
+	/// <remarks>
+	/// Used when several CVars share the same execution method. Such as a crosshair that reconstructs in its entirety no matter which CVar changed.
+	/// </remarks>
+	public event Action? ValueInvalidated;
+
+	// Stupid Godot ruining my DRY code...
+	/// <summary>A property that exists solely for serializing the value to the editor.</summary>
+	/// <remarks>A hack we must use to make Godot compile, sadly.</remarks>
+	protected abstract T DefaultValueEditor { get; set; }
+
+	/// <summary>A property that exists solely for serializing the value to the editor.</summary>
+	/// <remarks>A hack we must use to make Godot compile, sadly.</remarks>
+	protected abstract T ValueEditor { get; set; }
+
+	[ExportGroup("CVar")]
+	[Export] public bool Persist { get; private set; } = false;
+	[Export] public bool IsCheat { get; private set; } = false;
+
+
+	// Set automatic
+	public string Signature { get; private set; } = string.Empty;
+	public virtual string ShortDesc => $"View or set the value of {Signature}";
+	public virtual string Usage => $"{Signature} [new value]";
+
+	// Set in child
+	public abstract string DisplayType { get; }
+	public abstract string LongDesc { get; }
+
+	// Current value getter / setter
+	public T Value
+	{
+		get => ValueEditor;
+		set
+		{
+			if (!EqualityComparer<T>.Default.Equals(ValueEditor, value))
+			{
+				ValueEditor = value;
+				ValueChanged?.Invoke(value);
+				ValueInvalidated?.Invoke();
+			}
+		}
+	}
+
+	/// <summary>
+	/// This is the method used by the console to set the command.
+	/// It is responsible for parsing the arguments and setting the value.
+	/// </summary>
+	/// <param name="args">Arguments passed by the runtime console</param>
+	/// <returns>A response status with an optional message.</returns>
+	public abstract Response<CvarSetResponseStatus> SetValue(string[] args);
+
+	/// <summary>
+	/// InitializeInternal is called by the CVar crawler when the resource is loaded into memory.
+	/// It is self diagnostic and will log errors or warnings regarding registering to the command registry.
+	/// </summary>
+	/// <remarks>DO NOT CALL THIS METHOD. IT IS PUBLIC ONLY SO THE CRAWLER CAN FIND IT!</remarks>
+	public void InitializeInternal()
+	{
+		ValueEditor = DefaultValueEditor;
+		// Note: This causes interop overhead at startup. That's okay and unavoidable.
+		// The resource filename IS the command name. Convenience vs customization and all that.
+		Signature = ResourcePath.GetFile().GetBaseName().ToSignature();
+
+		if (IsCheat)
+			PikeConsoleConfig.CheatModeChanged += OnCheatModeChanged;
+
+		var response = CommandRegistry.Register(this);
+
+		switch (response.Status)
+		{
+			case RegisterCommandResponseStatus.Success:
+				break;
+			case RegisterCommandResponseStatus.AlreadyExists:
+				PikeLogger.LogError(LogTarget.All, $"CVar {Signature} couldn't register as a command or CVar of this name already exists!");
+				break;
+			case RegisterCommandResponseStatus.ReplacedAlias:
+				PikeLogger.LogWarning(LogTarget.All, $"CVar {Signature} was registered, but replaced an alias with th same signature.");
+				break;
+			default:
+				PikeLogger.LogError(LogTarget.All, $"CVar {Signature} didn't get a valid response from the command registry.");
+				break;
+		}
+	}
+
+	// 99% of the time, resources live from start to end and this wont be needed.
+	// Its added for good hygiene and memory leak prevention during exceptional circumstances
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && IsCheat)
+			PikeConsoleConfig.CheatModeChanged -= OnCheatModeChanged;
+
+		base.Dispose(disposing);
+	}
+
+	private void OnCheatModeChanged(bool cheatMode)
+	{
+		if (!cheatMode)
+		{
+			Value = DefaultValueEditor;
+		}
+	}
+
+	public void ResetValue()
+	{
+		Value = DefaultValueEditor;
+		// TODO: Add RemoveFromConfig() (once config and file system is in place)
+	}
+
+	// ----- ----- Note to future self: 
+	// The StatementExecutor will be able to differ Commands from CVars. 
+	// CVar response text will not be green unless we want to.
+	// Success is a state, not an aesthetic! Calm down.
+	// 
+	// Also, remember that if no message is returned, there will be no log!
+	// ----- -----
+	public Response<ExecutionResponseStatus> Execute(string[] args)
+	{
+		// No arguments mean we want to check the value.
+		// Early return with success message for the current value.
+		if (args.Length < 1 || string.IsNullOrWhiteSpace(args[0]))
+		{
+			string currentValue = ValueEditor != null ? DisplayValue(ValueEditor) : "null";
+			string defaultValue = DefaultValueEditor != null ? DisplayValue(DefaultValueEditor) : "null";
+
+			return new(ExecutionResponseStatus.Success, $"Current value: {currentValue}\nDefault value: {defaultValue}\nIs cheat: {IsCheat}");
+		}
+
+		// If we are in cheat mode, 
+		if (IsCheat && !PikeConsoleConfig.CheatMode)
+			return new(ExecutionResponseStatus.DeniedCheat, null);
+
+		bool isNosave = args.Length >= 2 && args[^1].Equals("nosave", StringComparison.OrdinalIgnoreCase);
+
+		Response<CvarSetResponseStatus> response;
+
+		try
+		{
+			response = SetValue(isNosave ? args[..^1] : args);
+		}
+		catch (Exception e)
+		{
+			return new(ExecutionResponseStatus.Error, $"An unexpected error occured when setting value of {Signature}: {e.Message}");
+		}
+
+		// Return success, but log nothing.
+		if (response.Status == CvarSetResponseStatus.NoChange)
+			return new(ExecutionResponseStatus.Success, null);
+
+		if (response.Status == CvarSetResponseStatus.Success)
+		{
+			if (Persist)
+			{
+				SaveToMemory();
+				if (!isNosave)
+				{
+					// TODO: Add ConfigManager.SaveToDiskDebounce() (once config and file system is in place)
+					// REQUIREMENTS: ConfigManager
+					// ConfigManager.SaveToDiskDebounce writes the entire current config to disk. 
+					// It uses debounce so that aliases and other fast acting scripts doesn't spam it.
+				}
+			}
+			string msg = response.Message ?? $"{Signature} set to {Value}";
+			return new(ExecutionResponseStatus.Success, msg);
+		}
+
+		return response.Status switch
+		{
+			CvarSetResponseStatus.InvalidArgs => new(ExecutionResponseStatus.InvalidArgs, response.Message ?? $"Invalid arguments passed for {Signature}."),
+			CvarSetResponseStatus.Failed => new(ExecutionResponseStatus.Failed, response.Message ?? $"Failed to set the value for {Signature}"),
+			_ => new(ExecutionResponseStatus.Error, response.Message ?? $"An unexpected error occured when setting the value for {Signature}"),
+		};
+	}
+
+	public string GetHelp()
+	{
+		// TODO: Create a utility to standarize help messages...
+		throw new System.NotImplementedException();
+	}
+
+	// Generic helpers
+	/// <summary>
+	/// Override for how this value should be displayed in text.
+	/// </summary>
+	/// <remarks>
+	/// Overriding the "ToString" property for the value is not adviced as that could ruin parsing.
+	/// <remarks>
+	/// <returns>Value as string</returns>
+	public virtual string DisplayValue(T value) => value?.ToString() ?? "null";
+
+	/// <summary>
+	/// Save this CVar to RAM.
+	/// </summary>
+	void SaveToMemory()
+	{
+		// IN MANAGER ... (string signature, T value, string comment = null)
+		// ConfigManager.SaveValue<T>(Signature, ValueEditor, DisplayType)
+		// This should save to config without having to override the logic. 
+		// The saving is opinionated and will look like: 
+		// m_sensitivity 1.5 // Float
+		// Signature ValueEditor.ToString() // DisplayType
+	}
+
+	void RemoveFromConfig()
+	{
+		if (Persist)
+		{
+			// TODO: Add config manager
+			// ConfigManager.RemoveValue(Signature);
+		}
+	}
+
+}
