285c3c1312c48f921b577dc02dc2a0782126d70c
VonRiddarn <vonriddarn@gmail.com>
2026-06-11 22:29:21 +0200

    fix: add missing configs

--- Core/PikeConsoleConfig.cs
+++ Core/PikeConsoleConfig.cs
@@ -14,6 +14,7 @@ public static class PikeConsoleConfig
 	// Since this is interop stuff that is being set from "pike_console.gd" everything MUST MATCH!!
 	// If there are import errors, check the definitions at: res://addons/PikeConsole/pike_console.gd -- This is the init script.
 	const string SETTINGS_ROOT = "fractal_pike/pike_console";
+	const string SETTINGS_CONFIG = $"{SETTINGS_ROOT}/config";
 	const string SETTINGS_RUNTIME = $"{SETTINGS_ROOT}/runtime";
 	const string SETTINGS_EDITOR = $"{SETTINGS_ROOT}/editor";
 	const string SETTINGS_EDITOR_COLORS = $"{SETTINGS_EDITOR}/colors";
@@ -25,7 +26,18 @@ public static class PikeConsoleConfig
 	public static string PathMap => _pathMap ??= ProjectSettings.GetSetting($"{SETTINGS_ROOT}/pathmap", "").AsString();
 
 	static string? _cvarDirectory = null;
-	public static string CvarDirectory => _cvarDirectory ??= ProjectSettings.GetSetting($"{SETTINGS_ROOT}/cvar_directory_relative", "res://cvars").AsString();
+	public static string CvarDirectory => _cvarDirectory ??= ProjectSettings.GetSetting($"{SETTINGS_ROOT}/cvar_directory", "res://cvars").AsString();
+
+	static string? _configDirectory = null;
+	public static string ConfigDirectory => _configDirectory ??= ProjectSettings.GlobalizePath(
+		ProjectSettings.GetSetting($"{SETTINGS_CONFIG}/config_directory", "user://cfg").AsString());
+
+	static string? _userConfigsDirectory = null;
+	public static string UserConfigsDirectory => _userConfigsDirectory ??= ProjectSettings.GlobalizePath(
+		ProjectSettings.GetSetting($"{SETTINGS_CONFIG}/config_directory", "user://cfg").AsString()) + "/users";
+
+	static bool? _userConfigsEnabled = null;
+	public static bool UserConfigsEnabled => _userConfigsEnabled ??= ProjectSettings.GetSetting($"{SETTINGS_CONFIG}/use_user_configs", false).AsBool();
 
 	static int? _maxUiLogs = null;
 	/// <summary>
@@ -35,7 +47,7 @@ public static class PikeConsoleConfig
 
 	static bool? _suppressDocumentationWarnings = null;
 	/// <summary>
-	/// If set to true, warnings about commands not containing propper documentation such as "usage" and "shortDesc" are supressed.
+	/// If set to true, warnings about commands not containing propper documentation such as "usage" and "shortDesc" are suppressed.
 	/// </summary>
 	public static bool SuppressDocumentationWarnings =>
 		_suppressDocumentationWarnings ??= ProjectSettings.GetSetting($"{SETTINGS_EDITOR}/suppress_documentation_warnings", false).AsBool();
@@ -65,7 +77,7 @@ public static class PikeConsoleConfig
 
 	public static event Action<bool>? CheatModeChanged;
 
-	// TOOD: CHEATMODE IS NOT A CVAR!!! NOTE TO SELF
+	// TODO: CHEATMODE IS NOT A CVAR!!! NOTE TO SELF
 	// BIG UPGRADE FROM THE UNITY FRAMEWORK.
 	// We will not force inject cheats as a CVar. It makes no sense to add such a fragile wrapper.
 	// Instead, we will register it as a COMMAND in the GlobalCommandSet. And that command will just affect this static variable.
@@ -92,8 +104,11 @@ public static class PikeConsoleConfig
 	public static string TestSettings() => $@"
 PathMap: {PathMap}
 CvarDirectory: {CvarDirectory}
+ConfigDirectory: {ConfigDirectory}
+UserConfigsEnabled: {UserConfigsEnabled}
+UserConfigsDirectory: {UserConfigsDirectory}
 MaxUiLogs: {MaxUiLogs}
-SupressDocumentationWarnings: {SuppressDocumentationWarnings}
+SuppressDocumentationWarnings: {SuppressDocumentationWarnings}
 InfoColor: {InfoColor.ToHtml(false)}
 SuccessColor: {SuccessColor.ToHtml(false)}
 WarningColor: {WarningColor.ToHtml(false)}
