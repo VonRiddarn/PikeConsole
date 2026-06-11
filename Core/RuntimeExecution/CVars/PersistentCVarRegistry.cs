2fe044dc89b68cca810c158397bbee5d5cd150eb
VonRiddarn <vonriddarn@gmail.com>
2026-06-11 19:32:46 +0200

    feat: prepare registry for persistent CVars

---
+++ Core/RuntimeExecution/CVars/PersistentCVarRegistry.cs
@@ -0,0 +1,14 @@
+using Godot;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+
+namespace FractalPike.PikeConsole.Core.RuntimeExecution;
+public static class PersistentCVarRegistry
+{
+	static Dictionary<string, ICVar> _persistentCvars = [];
+
+
+	// TODO: Add the logic...
+}
