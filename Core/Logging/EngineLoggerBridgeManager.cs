84040fae3377e64fb6e49fd5277d3d8a71229d3f
VonRiddarn <vonriddarn@gmail.com>
2026-06-14 00:16:30 +0200

    feat: add engine logger bridge manager - maps engine errors to logs

---
+++ Core/Logging/EngineLoggerBridgeManager.cs
@@ -0,0 +1,62 @@
+using Godot;
+
+namespace FractalPike.PikeConsole.Core.Logging;
+
+public partial class EngineLoggerBridgeManager : Node
+{
+	// Note: We need this cached so that the GC doesn't try to clean it.
+	// The GC can't see the reference from accross the interop.
+	EngineLoggerBridge _engineLogger = null;
+
+	public override void _EnterTree()
+	{
+		// CVar.ChangedValue += OnNewInteropValue;
+		ActivateInteropLogger();
+		GD.PushWarning("What's going on?");
+		GD.PushError("OH NO! THE HUMANITY!");
+	}
+
+	public override void _ExitTree()
+	{
+		KillInteropLogger();
+	}
+
+	void OnNewInteropValue(bool newValue)
+	{
+		if (newValue)
+			ActivateInteropLogger();
+		else
+			KillInteropLogger();
+	}
+
+	// TODO: Create internal CVars that manage this at runtime. 
+	// NOTE TO SELF: If we create internal CVars we are also responsible for initializing them. Use [Export] for the CVars.
+	public void ActivateInteropLogger()
+	{
+		if (_engineLogger != null) // || console_inject_engine_logs (CVar) false
+			return;
+
+		_engineLogger = new EngineLoggerBridge();
+		OS.AddLogger(_engineLogger);
+
+		// TODO: Look into how Godot handles crashes (both engine and .NET environment)
+		// We could sub to AppDomain.CurrentDomain.UnhandledException here. 
+		// Though the UI would not be able to see it before the game dies, we 
+		// could still force-empty the buffer to some file for crash reports. 
+		// (Unless Godot already does that natively)
+
+		PikeLogger.Log(LogTarget.All, $"[PikeConsole] interop connection established. Engine exceptions and warnings are logged.");
+	}
+
+	public void KillInteropLogger()
+	{
+		if (_engineLogger == null) // || console_inject_engine_logs (CVar) true
+			return;
+
+		OS.RemoveLogger(_engineLogger);
+		_engineLogger.Dispose();
+		_engineLogger = null;
+
+		PikeLogger.Log(LogTarget.All, $"[PikeConsole] interop connection severed. Engine exceptions are no longer logged.");
+	}
+}
