b590f14c4e056fd92ff4ddef8b7786eecc24d655
VonRiddarn <vonriddarn@gmail.com>
2026-06-14 15:26:03 +0200

    feat: add int CVar

---
+++ Core/RuntimeExecution/CVars/CVarInt.cs
@@ -0,0 +1,44 @@
+using FractalPike.PikeConsole.Core.RuntimeExecution.Cvars.Internal;
+using Godot;
+
+namespace FractalPike.PikeConsole.Core.RuntimeExecution.Cvars;
+
+[GlobalClass]
+public partial class CVarInt : CVarBase<int>
+{
+	public override string DisplayType => "CVar_Int";
+
+	[Export]
+	protected override int _defaultValue { get; set; }
+	[Export]
+	protected override int _value { get; set; }
+
+	[ExportGroup("Limits")]
+	[ExportSubgroup("Minimum")]
+	[Export] bool _useMin = false;
+	[Export] int _minValue = 0;
+	[ExportSubgroup("Maximum")]
+	[Export] bool _useMax = false;
+	[Export] int _maxValue = 0;
+
+	public override Response<CvarSetResponseStatus> SetValue(string[] args)
+	{
+		if (!ArgumentParser.ValidateCount(args, 1, out string error))
+			return new(CvarSetResponseStatus.InvalidArgs, error);
+
+		if (!int.TryParse(args[0], out int i))
+			return new(CvarSetResponseStatus.Failed, $"Can not convert {args[0]} to type int.");
+
+		if (i == Value)
+			return new(CvarSetResponseStatus.NoChange, null);
+
+		if (_useMin && i < _minValue)
+			return new(CvarSetResponseStatus.Failed, $"Cannot set {Signature} to anything less than {_minValue}.");
+
+		if (_useMax && i > _maxValue)
+			return new(CvarSetResponseStatus.Failed, $"Cannot set {Signature} to anything higher than {_maxValue}.");
+
+		Value = i;
+		return new(CvarSetResponseStatus.Success, null);
+	}
+}
