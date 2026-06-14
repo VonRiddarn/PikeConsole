446844f656d8e64f0153639fb6a3bce55628597e
VonRiddarn <vonriddarn@gmail.com>
2026-06-14 12:22:59 +0200

    feat: ADD FIRST RESOURCE (Bool) NOTES: IT'S ALIVE!!! IT LIIIIVES!!!

--- Core/RuntimeExecution/CVars/CVarBool.cs
+++ Core/RuntimeExecution/CVars/CVarBool.cs
@@ -1,8 +1,30 @@
+using FractalPike.PikeConsole.Core.RuntimeExecution.Cvars.Internal;
 using Godot;
 using System;
 
 namespace FractalPike.PikeConsole.Core.RuntimeExecution.Cvars;
 
-public partial class CVarBool : Node
+[GlobalClass]
+public partial class CVarBool : CVarBase<bool>
 {
+	public override string DisplayType => "CVar_Bool";
+
+	[Export]
+	protected override bool DefaultValueEditor { get; set; }
+
+	[Export]
+	protected override bool ValueEditor { get; set; }
+
+	public override Response<CvarSetResponseStatus> SetValue(string[] args)
+	{
+		if (!ArgumentParser.ValidateCount(args, 1, out string error))
+			return new(CvarSetResponseStatus.InvalidArgs, error);
+
+		if (!ArgumentParser.TryParseBool(args[0], out bool value))
+			return new(CvarSetResponseStatus.Failed, $"Could not parse {args[0]} into type bool.");
+
+		Value = value;
+		return new(CvarSetResponseStatus.Success, null);
+
+	}
 }
