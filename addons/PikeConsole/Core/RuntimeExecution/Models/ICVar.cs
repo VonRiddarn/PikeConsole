327506ce22ac32d003962a36a0612a2349d067aa
VonRiddarn <vonriddarn@gmail.com>
2026-06-15 23:48:42 +0200

    feat: add Initialize to the CVar interface for type-agnostic registration

--- addons/PikeConsole/Core/RuntimeExecution/Models/ICVar.cs
+++ addons/PikeConsole/Core/RuntimeExecution/Models/ICVar.cs
@@ -4,9 +4,12 @@ public interface ICVar : IRuntimeExecutable
 	public bool Persist { get; }
 	public void ResetValue();
 
+	/// <summary>Used by the individual CVar to register itself to the registry.</summary>
+	public void Initialize();
+
 	// These are mostly for routing / saving the data.
 	public bool IsModified { get; }
 	/// <summary>The string formated value. This value MUST be passable to the SetValue method (note that spaces become separate args).</summary>
-	/// <remarks>By default, this just reutns the values ToString method.</remarks>
+	/// <remarks>By default, this just returns the values ToString method.</remarks>
 	public string FormattedValue { get; }
 }
