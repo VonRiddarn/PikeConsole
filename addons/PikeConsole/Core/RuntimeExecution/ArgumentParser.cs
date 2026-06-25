6373c056a1892db13b1e4cdda2591864cd93412a
VonRiddarn <vonriddarn@gmail.com>
2026-06-26 00:00:56 +0200

    feat: expand argument parser

--- addons/PikeConsole/Core/RuntimeExecution/ArgumentParser.cs
+++ addons/PikeConsole/Core/RuntimeExecution/ArgumentParser.cs
@@ -4,7 +4,16 @@ namespace FractalPike.PikeConsole.Core.RuntimeExecution;
 public static class ArgumentParser
 {
 	public static bool ValidateCount(ReadOnlySpan<string> args, int count, out string error)
-	=> ValidateCount(args, count, count, out error);
+	{
+		if (!ValidateCount(args, count, count, out error))
+		{
+			error = $"{error} Argument count must be exactly {count}.";
+			return false;
+		}
+
+		error = string.Empty;
+		return true;
+	}
 
 	public static bool ValidateCount(ReadOnlySpan<string> args, int min, int max, out string error)
 	{
@@ -26,10 +35,31 @@ public static class ArgumentParser
 		return true;
 	}
 
+	public static bool ValidateCount(ReadOnlySpan<string> args, ReadOnlySpan<int> counts, out int count, out string error)
+	{
+		int len = args.Length;
+		count = -1;
+
+		for (int i = 0; i < counts.Length; i++)
+		{
+			if (len == counts[i])
+			{
+				count = len;
+				error = string.Empty;
+				return true;
+			}
+		}
+
+		error = $"Invalid argument count. Expected one of: {string.Join(", ", counts.ToArray())}";
+		return false;
+	}
+
 	// ----- ----- CUSTOM PARSING ----- -----
 	// CVar 
-	public static bool TryParseBool(ReadOnlySpan<char> input, out bool value)
+	public static bool TryParseBool(ReadOnlySpan<char> input, out bool value, out string error)
 	{
+		error = string.Empty;
+
 		// Note: In the Unity version we tried parsing to int. That's a complete waste.
 		// There's no need to "check if the number is a number" as we don't even use it for math.
 		if (input is "1" || input.Equals("true", StringComparison.OrdinalIgnoreCase))
@@ -43,13 +73,14 @@ public static class ArgumentParser
 			return true;
 		}
 
+		error = $"Can't parse {input} to a boolean value!";
 		value = false;
 		return false;
 	}
 
 	public static bool TryParseEnum(ReadOnlySpan<char> input, ReadOnlySpan<string> options, out int index, out string error)
 	{
-		error = null;
+		error = string.Empty;
 		index = -1;
 
 		// Try parsing int first as that is the fastest path. Hopecore.
@@ -100,21 +131,50 @@ public static class ArgumentParser
 	static readonly TryParseDelegate<double> invariantDoubleParser = (string s, out double d) =>
 		double.TryParse(s, System.Globalization.CultureInfo.InvariantCulture, out d);
 
+	// ----- ----- TRYPARSEMANY WRAPPERS GOES HERE ----- -----
+
+	/*
+	 * Originally I was going to leave the generic method exposed.
+	 * That idea was scrapped and replaced with these wrappers to reduce boilerplate 
+	 * and provide a better DX when using the parser.
+	 */
 
 	/// <summary>
-	/// Use this instead of "TryParseMany" for floats. It automatically applies culture invarience to floating point numbers. <br />
+	/// Use <c>args.AsSpan(start, end)</c> if the parameters are continuous. <br />
+	/// Use shorthand: <c>[args[1], args[3], args[7]]</c> if the parameters are non-continuous.
+	/// </summary>
+	public static bool TryParseManyInt(ReadOnlySpan<string> args, out int[] values, out string error) =>
+	TryParseManyInternal(int.TryParse, args, out values, out error);
+	/// <summary>
 	/// Use <c>args.AsSpan(start, end)</c> if the parameters are continuous. <br />
 	/// Use shorthand: <c>[args[1], args[3], args[7]]</c> if the parameters are non-continuous.
 	/// </summary>
 	public static bool TryParseManyFloat(ReadOnlySpan<string> args, out float[] values, out string error) =>
-	TryParseMany(invariantFloatParser, args, out values, out error);
+	TryParseManyInternal(invariantFloatParser, args, out values, out error);
 	/// <summary>
-	/// Use this instead of "TryParseMany" for doubles. It automatically applies culture invarience to floating point numbers. <br />
 	/// Use <c>args.AsSpan(start, end)</c> if the parameters are continuous. <br />
 	/// Use shorthand: <c>[args[1], args[3], args[7]]</c> if the parameters are non-continuous.
 	/// </summary>
 	public static bool TryParseManyDouble(ReadOnlySpan<string> args, out double[] values, out string error) =>
-		TryParseMany(invariantDoubleParser, args, out values, out error);
+		TryParseManyInternal(invariantDoubleParser, args, out values, out error);
+
+	/// <summary>
+	/// Use <c>args.AsSpan(start, end)</c> if the parameters are continuous. <br />
+	/// Use shorthand: <c>[args[1], args[3], args[7]]</c> if the parameters are non-continuous.
+	/// </summary>
+	public static bool TryParseManyBool(ReadOnlySpan<string> args, out bool[] values, out string error) =>
+		TryParseManyInternal((string s, out bool b) => TryParseBool(s.AsSpan(), out b, out _), args, out values, out error);
+
+	/// <summary>
+	/// Use <c>args.AsSpan(start, end)</c> if the parameters are continuous. <br />
+	/// Use shorthand: <c>[args[1], args[3], args[7]]</c> if the parameters are non-continuous.
+	/// </summary>
+	public static bool TryParseManyEnum(ReadOnlySpan<string> args, string[] options, out int[] values, out string error) =>
+		TryParseManyInternal((string s, out int idx) => TryParseEnum(s.AsSpan(), options, out idx, out _), args, out values, out error);
+
+	// TODO: Add Godot related stuff like the Color, Vector2 and Vector3 here later...
+
+	// ----- ----- WRAPPERS ABOVE THIS LINE! ----- -----
 
 	/// <summary>
 	/// Takes a parser method, such as "int.TryParse" and automatically maps it to parse all arguments in an array. <br />
@@ -131,7 +191,7 @@ public static class ArgumentParser
 	/// <param name="error"></param>
 	/// <typeparam name="T"></typeparam>
 	/// <returns>Success: true and fills "values" || Fail: false and fills "error"</returns>
-	public static bool TryParseMany<T>(TryParseDelegate<T> parser, ReadOnlySpan<string> args, out T[] values, out string error)
+	static bool TryParseManyInternal<T>(TryParseDelegate<T> parser, ReadOnlySpan<string> args, out T[] values, out string error)
 	{
 		if (args.Length <= 0)
 		{
@@ -153,7 +213,7 @@ public static class ArgumentParser
 			}
 		}
 
-		error = null;
+		error = string.Empty;
 		return true;
 	}
 }
