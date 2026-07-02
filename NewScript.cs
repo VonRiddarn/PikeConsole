e57f18244dca7ebaca4401e3047b891e8289fb55
VonRiddarn <vonriddarn@gmail.com>
2026-07-02 23:34:30 +0200

    feat: add example stuff NOTE: This will be removed later - pushing so that it gets out of the way

---
+++ NewScript.cs
@@ -0,0 +1,46 @@
+using FractalPike.PikeConsole.Core.Logging;
+using FractalPike.PikeConsole.Core.RuntimeExecution;
+using FractalPike.PikeConsole.Core.RuntimeExecution.Commands;
+using Godot;
+
+public partial class CMDSET : CommandSet
+{
+	protected override void OnEnterTree()
+	{
+		PikeLogger.Log(LogTarget.All, $"Tree entered!");
+	}
+
+	protected override void OnReady()
+	{
+		PikeLogger.Log(LogTarget.All, $"Node ready!");
+	}
+	protected override void OnExitTree()
+	{
+		PikeLogger.Log(LogTarget.All, $"Tree exited!");
+	}
+
+	protected override void OnCheatModeChanged(bool newState)
+	{
+		if (!newState)
+			PikeLogger.Log(LogTarget.All, $"Force removing noclip...");
+	}
+
+	protected override Command[] InstantiateCommands()
+	{
+		return [
+			Command(
+					"my_echo",
+					"Joins and echoes the arguments back to the caller",
+					null,
+					"my_echo [args...]",
+					false,
+					EchoCommand
+				),
+			];
+	}
+
+	Response<ExecutionResponseStatus> EchoCommand(string[] args)
+	{
+		return new(ExecutionResponseStatus.Success, $"{args.Join(" ")}");
+	}
+}
