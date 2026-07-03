562af54aa95c38085eba6c0d7c66dde7b62e87a9
VonRiddarn <vonriddarn@gmail.com>
2026-07-03 19:09:26 +0200

    feat: add some default commands transpiled from old Unity based framework

--- addons/PikeConsole/Frontend/ConsoleCommandSet.cs
+++ addons/PikeConsole/Frontend/ConsoleCommandSet.cs
@@ -1,6 +1,8 @@
+using FractalPike.PikeConsole.Core.Logging;
 using FractalPike.PikeConsole.Core.RuntimeExecution;
 using FractalPike.PikeConsole.Core.RuntimeExecution.Commands;
-using FractalPike.PikeConsole.Frontend;
+
+namespace FractalPike.PikeConsole.Frontend;
 
 public partial class ConsoleCommandSet : CommandSet
 {
@@ -25,6 +27,77 @@ public partial class ConsoleCommandSet : CommandSet
 				_pikeConsoleUi.Clear();
 				return new(ExecutionResponseStatus.Success, null);
 			}
-		)
+		),
+		Command(
+			"echo",
+			"Send a message to the console.",
+			"Combines all arguments into a string and returns the concatenated result.",
+			"echo [..args]",
+			false,
+			(args) => {
+				PikeLogger.Log(LogTarget.Runtime, $"{string.Join(' ', args)}", forceLog: true, domain: "PikeConsole.Frontend");
+				return new(ExecutionResponseStatus.Success, null);
+			}
+		),
+		Command(
+			"count",
+			"Count all passed arguments.",
+			"Counts all arguments and logs an integer of the count.",
+			"count [..args]",
+			false,
+			(args) => {
+				PikeLogger.Log(LogTarget.Runtime, $"{args.Length.ToString()}", forceLog: true, domain: "PikeConsole.Frontend");
+				return new(ExecutionResponseStatus.Success, null);
+			}
+		),
+		Command(
+			"help",
+			"Get detailed help of any command or CVar.",
+			null,
+			"help [signature]",
+			false,
+			(args) => {
+				if(!ArgumentParser.ValidateCount(args, 1, out string error))
+					return new(ExecutionResponseStatus.InvalidArgs, error);
+
+				string signature = args[0];
+
+				if(RuntimeExecutableRegistry.TryGetExecutable(signature, out var rte))
+				{
+					PikeLogger.Log(LogTarget.Runtime, $"{rte.GetHelp()}", forceLog: true, domain: "PikeConsole.Frontend");
+					return new(ExecutionResponseStatus.Success, null);
+				}
+
+				return new(ExecutionResponseStatus.Failed, $"Could not find runtime executable with signature \"{signature}\".");
+			}
+		),
+		Command(
+			"reset",
+			"Reset the value of a CVar.",
+			"Reset the value of a CVar and remove persistance overrides from the player settings config.",
+			"reset [signature]",
+			false,
+			(args) => {
+				if(!ArgumentParser.ValidateCount(args, 1, out string error))
+					return new(ExecutionResponseStatus.InvalidArgs, error);
+
+				string signature = args[0];
+
+				if(RuntimeExecutableRegistry.TryGetExecutable(signature, out var rte))
+				{
+					if(rte is ICVar cvar)
+					{
+						if(!cvar.ResetValue(ExecutionSource.Player))
+							return new(ExecutionResponseStatus.DeniedCheat, $"Failed to reset value of \"{cvar.Signature}\". CVar is cheat protected.");
+
+							return new(ExecutionResponseStatus.Success,$"\"{cvar.Signature}\" has been reset.");
+					}
+
+					return new(ExecutionResponseStatus.Failed, $"\"{rte.Signature}\" is not a CVar.");
+				}
+
+				return new(ExecutionResponseStatus.Failed, $"Unknown signature \"{signature}\".");
+			}
+		),
 	];
 }
