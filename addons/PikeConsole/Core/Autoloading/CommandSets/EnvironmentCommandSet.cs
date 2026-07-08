0ed0063e0daacc5f39a546f70ddcd8f11a2ff033
VonRiddarn <vonriddarn@gmail.com>
2026-07-08 17:25:13 +0200

    feat / fix: remove unnecessary optimization for readadability - add time command - add gc command

--- addons/PikeConsole/Core/Autoloading/CommandSets/EnvironmentCommandSet.cs
+++ addons/PikeConsole/Core/Autoloading/CommandSets/EnvironmentCommandSet.cs
@@ -7,38 +7,27 @@ using Godot;
 
 namespace FractalPike.PikeConsole.Core.Autoloading;
 
+/*
+ * Post optimization clarity (NOTE TO SELF):
+ * There is no need, in this cold path, to cache all interop values just to save a few nanoseconds.
+ * It just made the code unreadable. Do not reintroduce.
+*/
+
 public partial class EnvironmentCommandSet : CommandSet
 {
-	string _projectName = "GAME NAME NOT INITIALIZED";
-	string _cpuName = "CPU INFO NOT INITIALIZED";
-	string _osName = "OS INFO NOT INITIALIZED";
-	string _systemRam = "SYSTEM RAM INFO NOT INITIALIZED";
-	string _godotVersion = "GODOT VERSION INFO NOT INITIALIZED";
-	string _videoAdapter = "VIDEO ADAPTER INFO NOT INITIALIZED";
-	string _videoAdapterApi = "VIDEO ADAPTER API INFO NOT INITIALIZED";
+	static string DisplayBytes(double bytes, bool asMB = true) => asMB ?
+		$"{bytes / 1_048_576f:F2} MB ({bytes / 1_073_741_824f:F2} GB)"
+		: $"{bytes / 1_073_741_824f:F2} GB";
 
-	const string PREFIX = "env";
-
-	static string DisplayBytes(double bytes) => $"{bytes / 1_048_576f:F2} MB ({bytes / 1_073_741_824f:F2} GB)";
-
-	protected override void OnEnterTree()
+	static string GetProjectAndVersion()
 	{
-		// Go through all the interop stuff and cache everything inside the .NET runtime
 		string n = ProjectSettings.GetSetting("application/config/name").AsString();
 		string v = ProjectSettings.GetSetting("application/config/version").AsString();
 		v = string.IsNullOrWhiteSpace(v) ? "" : $"({v})";
-
-		_projectName = $"{n} {v}";
-		_osName = OS.GetName();
-		_cpuName = OS.GetProcessorName();
-		_godotVersion = Engine.GetVersionInfo()["string"].AsString();
-		_videoAdapter = RenderingServer.GetVideoAdapterName();
-		_videoAdapterApi = RenderingServer.GetVideoAdapterApiVersion();
-
-		long physicalBytes = (long)OS.GetMemoryInfo()["physical"];
-		_systemRam = $"{physicalBytes / 1_073_741_824f:F2} GB";
+		return $"{n} {v}";
 	}
 
+	const string PREFIX = "env";
 	protected override Command[] InstantiateCommands() => [
 		Command(
 			$"{PREFIX}_info",
@@ -47,25 +36,25 @@ public partial class EnvironmentCommandSet : CommandSet
 			$"{PREFIX}_info [no args]",
 			false,
 			(_) => {
-				StringBuilder sb = new($"{_projectName}");
-				sb.AppendLine($"Godot version: {_godotVersion}");
-				sb.AppendLine($"OS: {_osName} | RAM: {_systemRam}");
-				sb.AppendLine($"CPU: {_cpuName}");
-				sb.Append($"GPU: {_videoAdapter} [API: {_videoAdapterApi}]");
+				StringBuilder sb = new($"-- {GetProjectAndVersion()}\n");
+				sb.AppendLine($"Godot version: {Engine.GetVersionInfo()["string"].AsString()}");
+				sb.AppendLine($"OS: {OS.GetName()} | RAM: {DisplayBytes((long)OS.GetMemoryInfo()["physical"], false)}");
+				sb.AppendLine($"CPU: {OS.GetProcessorName()}");
+				sb.Append($"GPU: {RenderingServer.GetVideoAdapterName()} [API: {RenderingServer.GetVideoAdapterApiVersion()}]");
 
-				PikeLogger.Log(LogTarget.Runtime, $"{sb.ToString()}");
+				PikeLogger.Log(LogTarget.Runtime, $"{sb.ToString()}", forceLog: true);
 				return new(ExecutionResponseStatus.Success, null);
 			}
 		),
 		Command(
 			$"{PREFIX}_mem",
 			"Log a snapshot of the environments memory usage at this time.",
-			"Shows all relevant memory information as is. Crosses interop bridge to collect live-diagnostic data for the snapshot.",
+			"Shows all relevant memory information as is. Provides live-diagnostic data for the snapshot.",
 			$"{PREFIX}_mem [no args]",
 			false,
 			(_) => {
-				StringBuilder sb = new($"Memory snapshot\n");
-				sb.AppendLine($"System RAM: {_systemRam}");
+				StringBuilder sb = new($"-- Memory snapshot\n");
+				sb.AppendLine($"System RAM: {DisplayBytes((long)OS.GetMemoryInfo()["physical"], false)}");
 
 				double usedVramBytes = Performance.GetMonitor(Performance.Monitor.RenderVideoMemUsed);
 				sb.AppendLine($"VRAM Used: {DisplayBytes(usedVramBytes)}.");
@@ -76,9 +65,53 @@ public partial class EnvironmentCommandSet : CommandSet
 				long csharpRamBytes = GC.GetTotalMemory(false);
 				sb.Append($"Used (.NET): {DisplayBytes(csharpRamBytes)}");
 
-				PikeLogger.Log(LogTarget.Runtime, $"{sb.ToString()}");
+				PikeLogger.Log(LogTarget.Runtime, $"{sb.ToString()}", forceLog: true);
 				return new(ExecutionResponseStatus.Success, null);
 			}
 		),
+		Command(
+			$"{PREFIX}_gc",
+			"Performs a manual garbage collection.",
+			"Forces the garbage collector to run. Used for debugging and testing memory allocation.",
+			$"{PREFIX}_gc [no args]",
+			false,
+			(_) => {
+				try{
+					GC.Collect();
+				}
+				catch(Exception err)
+				{
+					return new(ExecutionResponseStatus.Error, $"Failed to collect garbage: {err.Message}");
+				}
+				return new(ExecutionResponseStatus.Success, "C# Garbage collected.");
+			}
+		),
+		Command(
+			$"{PREFIX}_time",
+			"Log a snapshot of the environments time context at this time.",
+			"Shows all relevant time information as is. Provides live-diagnostic data for the snapshot.",
+			$"{PREFIX}_time [no args]",
+			false,
+			(_) => {
+				StringBuilder sb = new("-- Time snapshot\n");
+				sb.AppendLine($"{DateTime.Now}");
+
+				// Uptime for the actual system running the game.
+				TimeSpan uptime = TimeSpan.FromMilliseconds(System.Environment.TickCount64);
+				sb.AppendLine($"System Uptime: {uptime.Hours:D2}h {uptime.Minutes:D2}m {uptime.Seconds:D2}s");
+
+				// Uptime for the actual C++ engine.
+				double timeInSeconds = Time.GetTicksMsec() / 1000.0;
+				int frames = Engine.GetFramesDrawn();
+				sb.AppendLine($"Godot Uptime: {timeInSeconds:F2}");
+				sb.AppendLine($"In Frames: {frames}");
+				sb.AppendLine($"Avg FPS: {Math.Floor(frames / timeInSeconds)}");
+
+				sb.Append($"Scale: {Engine.TimeScale}");
+
+				PikeLogger.Log(LogTarget.Runtime, $"{sb.ToString()}", forceLog: true);
+				return new(ExecutionResponseStatus.Success, null);
+			}
+		)
 	];
 }
