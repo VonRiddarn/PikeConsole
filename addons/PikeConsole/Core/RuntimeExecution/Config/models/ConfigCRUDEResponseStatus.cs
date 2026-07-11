e897c25676a4fccd77fc98ac90a0133b9590baa8
VonRiddarn <vonriddarn@gmail.com>
2026-07-11 14:21:41 +0200

    feat: add config response statuses

---
+++ addons/PikeConsole/Core/RuntimeExecution/Config/models/ConfigCRUDEResponseStatus.cs
@@ -0,0 +1,19 @@
+namespace FractalPike.PikeConsole.Core.RuntimeExecution.Config;
+
+public enum ConfigCRUDEResponseStatus
+{
+	/// <summary>Default fallback.</summary>
+	None = 0,
+	/// <summary>Executed expectedly.</summary>
+	Success = 1,
+	/// <summary>Denied because of cheat flag.</summary>
+	InvalidArgs = 2,
+	/// <summary>A required file was not found.</summary>
+	NotFound = 3,
+	/// <summary>A conflicting file exists that prevents the action.</summary>
+	FileConflict = 4,
+	/// <summary>Failed expectedly (IE: Through gameplay context).</summary>
+	Failed = 5,
+	/// <summary>Failed unexpectedly.</summary>
+	Error = 6
+}
