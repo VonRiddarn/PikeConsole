1ef1482350daa8c01ee20a3f74959ca628920aca
VonRiddarn <vonriddarn@gmail.com>
2026-07-20 01:42:47 +0200

    refactor: move commands designed for testing pikeconsole out of the addon and into the example root reason: we want to show off these commands in a vacuum, but we don't want them to ship with the console for anyone downloading the plugin

--- addons/PikeConsole/Core/Autoloading/CommandSets/DiagnosticCommandSet.cs
+++ addons/PikeConsole/Core/Autoloading/CommandSets/DiagnosticCommandSet.cs
@@ -1,7 +1,6 @@
 using FractalPike.PikeConsole.Core.Logging;
 using FractalPike.PikeConsole.Core.RuntimeExecution;
 using FractalPike.PikeConsole.Core.RuntimeExecution.Commands;
-using Godot;
 
 namespace FractalPike.PikeConsole.Core.Autoloading;
 
@@ -17,60 +16,6 @@ public partial class DiagnosticCommandSet : CommandSet
 			static (args) => {
 				PikeLogger.Log(LogTarget.Runtime, $"{string.Join(' ', args)}", forceLog: true);
 				return new(ExecutionResponseStatus.Success, null);
-			}
-		),
-		Command(
-			"echo_target",
-			"Send a message to a specific LogTarget.",
-			"Combines all arguments after the first into a string and logs the concatenated result to a specific environment. Used to test killswitches.",
-			"echo_target [runtime | debug | editor | all] [..args]",
-			false,
-			static (args) => {
-				if (args.Length < 2)
-					return new(ExecutionResponseStatus.InvalidArgs, "Usage: echo [target] [message]");
-
-				LogTarget target = args[0].ToLower() switch {
-					"runtime" => LogTarget.Runtime,
-					"editor" => LogTarget.Editor,
-					"debug" => LogTarget.Debug,
-					"all" => LogTarget.All,
-					_ => LogTarget.Runtime,
-				};
-
-				PikeLogger.Log(target, $"{string.Join(' ', args[1..])}", forceLog: true);
-				return new(ExecutionResponseStatus.Success, null);
-			}
-		),
-		Command(
-			"push_warning",
-			"Push a warning to the Godot engine using GD.PushWarning. Used to test interop logger.",
-			"Combines all arguments into a string and pushes it to the Godot engine as a warning.",
-			"push_warning [..args]",
-			false,
-			static (args) => {
-				GD.PushWarning(string.Join(' ', args));
-				return new(ExecutionResponseStatus.Success, null);
-			}
-		),
-		Command(
-			"push_error",
-			"Push a warning to the Godot engine using GD.PushError. Used to test interop logger.",
-			"Combines all arguments into a string and pushes it to the Godot engine as an error.",
-			"push_error [..args]",
-			false,
-			static (args) => {
-				GD.PushError(string.Join(' ', args));
-				return new(ExecutionResponseStatus.Success, null);
-			}
-		),
-		Command(
-			"throw",
-			"Throw a generic, unhandled exception to be caught by the StatementExecutor.",
-			"Combines all arguments into a message and throws a generic error in the .NET runtime environment. Used for testing the try-catch, PathMap and UI formatting.",
-			"throw [..args]",
-			false,
-			static (args) => {
-				throw new System.Exception(string.Join(' ', args));
 			}
 		),
 		Command(
